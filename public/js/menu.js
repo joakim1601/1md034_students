eabf6c477187919c27f51f5d7efca7b3d0230b20
eskapisten <40369370+eskapisten@users.noreply.github.com>
2019-02-06 16:54:33 +0100

    Fixade unika burgare

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,33 +2,33 @@ let imageString = "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4409427.
 
 
 let burgers = [{
-    "name": "Ostburgare",
-    "kCal": 850,
+    "name": "Cheese & Pickle",
+    "kCal": 128,
     "hasLactose": true,
-    "hasGluten": false,
-    "image": imageString
+    "hasGluten": true,
+    "image": "https://pbs.twimg.com/profile_images/426083882702626816/FG2QC_q9.jpeg",
 }, {
-    "name": "Ostburgare",
-    "kCal": 850,
+    "name": "Cheese & Bacon",
+    "kCal": 256,
     "hasLactose": true,
     "hasGluten": false,
-    "image": imageString
+    "image": "https://is2-ssl.mzstatic.com/image/thumb/Purple115/v4/1b/27/1f/1b271fa4-e49d-719f-d000-9a4e8d2b5748/source/256x256bb.jpg",
 }, {
-    "name": "Ostburgare3",
-    "kCal": 850,
-    "hasLactose": true,
+    "name": "Turkey & Mayo",
+    "kCal": 512,
+    "hasLactose": false,
     "hasGluten": false,
-    "image": imageString
+    "image": "http://images.bigoven.com/image/upload/t_recipe-256/basic-turkey-burger-plus-a01596.jpg",
 }, {
-    "name": "Ostburgare4",
-    "kCal": 850,
-    "hasLactose": true,
-    "hasGluten": false,
-    "image": imageString
+    "name": "Cheese & Beets",
+    "kCal": 1024,
+    "hasLactose": false,
+    "hasGluten": true,
+    "image": "http://images.bigoven.com/image/upload/t_recipe-256/aussie-bum-burger-with-wedges-043b3b.jpg",
 }, {
-    "name": "Ostburgare5",
-    "kCal": 850,
+    "name": "Fish & Chips",
+    "kCal": 2048,
     "hasLactose": true,
-    "hasGluten": false,
-    "image": imageString
+    "hasGluten": true,
+    "image": "http://images.bigoven.com/image/upload/t_recipe-256/fish-burger-with-lemon-aioli.jpg",
 }, ];
